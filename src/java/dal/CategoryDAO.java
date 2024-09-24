b2f70516984ac16bf3e62cf7580e309a70bb4080
HaiDTHE186294 <haidthe186294@fpt.edu.vn>
2024-09-25 01:19:59 +0700

    final web of iter1

--- src/java/dal/CategoryDAO.java
+++ src/java/dal/CategoryDAO.java
@@ -7,31 +7,28 @@ import java.util.ArrayList;
 import java.util.List;
 import model.Category;
 
-
 public class CategoryDAO extends DBContext {
-    
+
     // Phương thức để lấy tất cả danh mục từ cơ sở dữ liệu
-public List<Category> getAllCategories() {
-    List<Category> categories = new ArrayList<>();
-    String sql = "SELECT CategoryID, icon, CategoryName, ParentCategoryID FROM Category";
-    
-    try (PreparedStatement ps = connection.prepareStatement(sql); ResultSet rs = ps.executeQuery()) {
-        while (rs.next()) {
-            String categoryID = rs.getString("CategoryID");
-            byte[] icon = rs.getBytes("icon");
-            String categoryName = rs.getString("CategoryName");
-            String parentCategoryID = rs.getString("ParentCategoryID");
-            
-            // Tạo đối tượng Category và thêm vào danh sách
-            categories.add(new Category(categoryID, icon, categoryName, parentCategoryID));
+    public List<Category> getAllCategories() {
+        List<Category> categories = new ArrayList<>();
+        String sql = "SELECT CategoryID, icon, CategoryName, ParentCategoryID FROM Category";
+
+        try (PreparedStatement ps = connection.prepareStatement(sql); ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) {
+                String categoryID = rs.getString("CategoryID");
+                byte[] icon = rs.getBytes("icon");
+                String categoryName = rs.getString("CategoryName");
+                String parentCategoryID = rs.getString("ParentCategoryID");
+
+                // Tạo đối tượng Category và thêm vào danh sách
+                categories.add(new Category(categoryID, icon, categoryName, parentCategoryID));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-    } catch (SQLException e) {
-        e.printStackTrace();
+
+        return categories;
     }
-    
-    return categories;
-}
-
-
 
 }
